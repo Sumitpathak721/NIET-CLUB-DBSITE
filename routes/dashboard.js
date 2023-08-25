b9da5248d6eca43c41ed4b69460feacf12b3df0b
Sumit Pathak <pathaksumit721@gmail.com>
2023-08-25 14:02:21 +0530

    update views configuration-2

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,18 +8,21 @@ const route=express()
 
 
 route.get("/",async(req,res)=>{
-    res.render("../templates/dashboard.ejs");
+    res.render("../views/dashboard.ejs");
 });
 
+route.get("/test",async(req,res)=>{
+    res.render("./admin.ejs");
+})
 route.get("/getAccessData",verifyToken,async(req,res)=>{
     if(req.body.validation.verified){
         if(req.body.validation.user.Access=="admin"){
             let clubs = await clubModel.find({},{name:1});
             let main = await mainModel.findOne({});
-            res.render("../templates/admin.ejs",{main:main,clubs:clubs,MAIN_DIR:process.env.MAIN_DIR});
+            res.render("../views/admin.ejs",{main:main,clubs:clubs,MAIN_DIR:process.env.MAIN_DIR});
         }else if(req.body.validation.user.Access=="clubAdmin"){
             let club = await clubModel.findOne({_id:req.body.validation.user.AccessID});
-            res.render("../templates/clubAdmin.ejs",{club:club,MAIN_DIR:process.env.MAIN_DIR});
+            res.render("../views/clubAdmin.ejs",{club:club,MAIN_DIR:process.env.MAIN_DIR});
         }else{
             res.sendStatus(403);
         }
